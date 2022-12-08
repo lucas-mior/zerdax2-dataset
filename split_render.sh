d64cdf42dddf616419a1e1a5f80a6cd078b28cc1
lucas-mior <lucas.mior@tutamail.com>
2022-12-08 13:45:31 -0300

    lt fens

--- split_render.sh
+++ split_render.sh
@@ -3,7 +3,8 @@
 i=0
 increment=1
 nfens="$(wc -l fens.txt | awk '{print $1}')"
-while [ $i -le $nfens ]; do
+nfens=1
+while [ $i -lt $nfens ]; do
     echo "i = $i"
     blender -b zerdax2_models.blend -P blender.py -- "$i" "$increment"
     i=$((i+increment))
