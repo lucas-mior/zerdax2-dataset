1bc3fcb10c4663cc36b77690fb188407520e54b8
lucas-mior <lucas.mior@tutamail.com>
2023-06-06 17:55:14 -0300

    style

--- misc.py
+++ misc.py
@@ -9,96 +9,48 @@ COLORS = {
 SYMBOLS = {
     -1: "1",
     0: "Board",
-    1: "K",
-    2: "Q",
-    3: "R",
-    4: "B",
-    5: "N",
-    6: "P",
-    7: "k",
-    8: "q",
-    9: "r",
-    10: "b",
-    11: "n",
-    12: "p"
+    1: "K", 2: "Q", 3: "R",
+    4: "B", 5: "N", 6: "P",
+    7: "k", 8: "q", 9: "r",
+    10: "b", 11: "n", 12: "p"
 }
 
 NAMES = {
     0: "Board",
-    1: "WhiteKing",
-    2: "WhiteQueen",
-    3: "WhiteRook",
-    4: "WhiteBishop",
-    5: "WhiteKnight",
-    6: "WhitePawn",
-    7: "BlackKing",
-    8: "BlackQueen",
-    9: "BlackRook",
-    10: "BlackBishop",
-    11: "BlackKnight",
-    12: "BlackPawn"
+    1: "WhiteKing", 2: "WhiteQueen", 3: "WhiteRook",
+    4: "WhiteBishop", 5: "WhiteKnight", 6: "WhitePawn",
+    7: "BlackKing", 8: "BlackQueen", 9: "BlackRook",
+    10: "BlackBishop", 11: "BlackKnight", 12: "BlackPawn"
 }
 
 PIECES = {
     "Board": "Board",
-    "K": "WhiteKing",
-    "Q": "WhiteQueen",
-    "R": "WhiteRook",
-    "B": "WhiteBishop",
-    "N": "WhiteKnight",
-    "P": "WhitePawn",
-    "k": "BlackKing",
-    "q": "BlackQueen",
-    "r": "BlackRook",
-    "b": "BlackBishop",
-    "n": "BlackKnight",
-    "p": "BlackPawn",
+    "K": "WhiteKing", "Q": "WhiteQueen", "R": "WhiteRook",
+    "B": "WhiteBishop", "N": "WhiteKnight", "P": "WhitePawn",
+    "k": "BlackKing", "q": "BlackQueen", "r": "BlackRook",
+    "b": "BlackBishop", "n": "BlackKnight", "p": "BlackPawn",
 }
 
 CLASSES = {
     "Board": 0,
-    "K": 1,
-    "Q": 2,
-    "R": 3,
-    "B": 4,
-    "N": 5,
-    "P": 6,
-    "k": 1,
-    "q": 2,
-    "r": 3,
-    "b": 4,
-    "n": 5,
-    "p": 6
+    "K": 1, "Q": 2, "R": 3,
+    "B": 4, "N": 5, "P": 6,
+    "k": 1, "q": 2, "r": 3,
+    "b": 4, "n": 5, "p": 6
 }
 
 NUMBERS = {
     "Board": 0,
-    "K": 1,
-    "Q": 2,
-    "R": 3,
-    "B": 4,
-    "N": 5,
-    "P": 6,
-    "k": 7,
-    "q": 8,
-    "r": 9,
-    "b": 10,
-    "n": 11,
-    "p": 12
+    "K": 1, "Q": 2, "R": 3,
+    "B": 4, "N": 5, "P": 6,
+    "k": 7, "q": 8, "r": 9,
+    "b": 10, "n": 11, "p": 12
 }
 
 AMOUNT = {
     'Board': [0, 1],
-    'K': [0, 1],
-    'Q': [0, 3],
-    'R': [0, 2],
-    'B': [0, 2],
-    'N': [0, 3],
-    'P': [0, 8],
-    'k': [0, 1],
-    'q': [0, 3],
-    'r': [0, 2],
-    'b': [0, 2],
-    'n': [0, 3],
-    'p': [0, 8],
+    'K': [0, 1], 'Q': [0, 3], 'R': [0, 2],
+    'B': [0, 2], 'N': [0, 3], 'P': [0, 8],
+    'k': [0, 1], 'q': [0, 3], 'r': [0, 2],
+    'b': [0, 2], 'n': [0, 3], 'p': [0, 8],
 }
